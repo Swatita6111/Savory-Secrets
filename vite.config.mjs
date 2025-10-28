ee61248e98a01eaa46e8db8f77bcf4e8bbc7ce48
Swatita Ray Nayak <165885803+Swatita6111@users.noreply.github.com>
2025-10-28 16:24:00 +0000

    deploy the code

---
+++ vite.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: ["7pyv7j-5173.csb.app"],
+  },
+});
